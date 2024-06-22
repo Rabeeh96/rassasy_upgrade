812bb0c550752296f61747c48806d05e220c6f7d
Rabeeh96 <rabeeh99amk@gmail.com>
2024-06-22 18:01:22 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/new_design/dashboard/mobile_section/view/invoice/invocie_list_mobile.dart

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -10,7 +10,7 @@ import 'package:intl/intl.dart';
 //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
 // }
 //////
- String appVersion = "1.1.29";
+ String appVersion = "1.1.30";
 double defaultScreenWidth=850;
 start(context) {
   Loader.show(context,
